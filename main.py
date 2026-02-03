308905a748939bc9c8302e481f85fcb8834c50df
Hi9841 <maor.azarov@gmail.com>
2026-02-03 13:13:24 +0300

    s3 functions

--- main.py
+++ main.py
@@ -2,7 +2,31 @@ import boto3
 import click
 import sys
 import os
+import uuid
 from botocore.exceptions import NoCredentialsError, ClientError
+
+MAX_RESOURCES = 2
+
+def get_global_count():
+    ec2 = boto3.client('ec2')
+    s3 = boto3.client('s3')
+
+    filters = [
+        {'Name': 'tag:CreatedBy', 'Values': ['Hi-platform-cli']},
+        {'Name': 'instance-state-name', 'Values': ['running']}
+    ]
+    ec2_response = ec2.describe_instances(Filters=filters)
+    ec2_count = sum(len(r['Instances']) for r in ec2_response['Reservations'])
+
+    s3_response = s3.list_buckets()
+    s3_count = 0
+    if 'Buckets' in s3_response:
+        for bucket in s3_response['Buckets']:
+            if bucket['Name'].startswith("hi-platform-cli"):
+                s3_count += 1
+    
+    return ec2_count + s3_count
+
 
 @click.group()
 def cli():
@@ -41,7 +65,14 @@ def create(type, os):
         click.echo("Please run 'python main.py configure' first.")
         sys.exit(1)
 
-    click.echo("AWS Configuration verified. checking limits...")
+    click.echo("AWS Configuration verified. Checking global limits...")
+    
+    total_count = get_global_count()
+
+    if total_count >= MAX_RESOURCES:
+        click.echo(f"Error: Global Limit reached. You have {total_count} resources running.")
+        sys.exit(1)
+
     ssm = boto3.client('ssm')
     if os == 'amazon-linux':
         param_name = '/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64'
@@ -50,20 +81,10 @@ def create(type, os):
     
     response = ssm.get_parameter(Name=param_name)
     ami_id = response['Parameter']['Value']
-    ec2 = boto3.client('ec2')
-    filters = [
-        {'Name': 'tag:CreatedBy', 'Values': ['Hi-platform-cli']},
-        {'Name': 'instance-state-name', 'Values': ['running']}
-    ]
-    instances = ec2.describe_instances(Filters=filters)
-    count = sum(len(r['Instances']) for r in instances['Reservations'])
-    
-    if count >= 2:
-        click.echo(f"Error: Hard cap reached. You have {count} running instances.")
-        sys.exit(1)
+    ec2_client = boto3.client('ec2')
 
-    click.echo(f"Limit OK. Total will be {count + 1}/2. Launching {os}...")    
-    ec2.run_instances(
+    click.echo(f"Limit OK ({total_count}/{MAX_RESOURCES}). Launching {os}...")    
+    ec2_client.run_instances(
         ImageId=ami_id,
         InstanceType=type,
         MinCount=1,
@@ -79,9 +100,6 @@ def create(type, os):
         ]
     )
     click.echo(f"Success! Launched {type} instance.")
-
-
-
 
 @ec2.command()
 @click.option('--start',is_flag=True, help="Start the instance")
@@ -177,8 +195,127 @@ def s3():
     pass
 
 @s3.command()
+@click.option('--pub', is_flag=True, help="Make bucket public")
+@click.option('--pri', is_flag=True, help="Make bucket private (default)")
+def create(pub, pri):
+    if pub and pri:
+        click.echo("Error: You cannot specify both --pub and --pri.")
+        sys.exit(1)
+        
+    access_type = 'public' if pub else 'private'
+
+    try:
+        boto3.client('sts').get_caller_identity()
+    except (NoCredentialsError, ClientError):
+        click.echo("Error: Unable to connect to AWS.")
+        click.echo("Please run 'python main.py configure' first.")
+        sys.exit(1)
+
+    click.echo("AWS Configuration verified. Checking global limits...")
+
+    total_count = get_global_count()
+
+    if total_count >= MAX_RESOURCES:
+        click.echo(f"Error: Global Limit reached. You have {total_count} resources running.")
+        sys.exit(1)        
+    
+    bucket_name = f"hi-platform-cli-storage-{str(uuid.uuid4())[:8]}"
+
+    click.echo(f"Limit OK ({total_count+1}/{MAX_RESOURCES}). Creating {access_type} Bucket {bucket_name}...")    
+
+    s3 = boto3.client('s3')
+    
+    s3.create_bucket(Bucket=bucket_name)
+
+    s3.put_bucket_tagging(
+        Bucket=bucket_name,
+        Tagging={
+            'TagSet': [
+                {'Key': 'CreatedBy', 'Value': 'Hi-platform-cli'}, 
+                {'Key': 'Name', 'Value': bucket_name}          
+                ]
+        }
+    )    
+
+    if pub:
+        s3.delete_public_access_block(Bucket=bucket_name)
+        
+        s3.put_bucket_ownership_controls(
+            Bucket=bucket_name,
+            OwnershipControls={
+                'Rules': [{'ObjectOwnership': 'BucketOwnerPreferred'}]
+            }
+        )
+        
+        s3.put_bucket_acl(Bucket=bucket_name, ACL='public-read')
+
+    click.echo(f"Success! Created {access_type} Bucket.")
+
+
+@s3.command()
+@click.option('--bucket', required=True, help="The Bucket Name")
+@click.option('--file', required=True, help="The file path to upload")
+def upload(bucket , file):
+    if not os.path.exists(file):
+        click.echo(f"Error: The file '{file}' does not exist")
+        sys.exit(1)
+
+    s3 = boto3.client('s3')
+    try:
+        tags = s3.get_bucket_tagging(Bucket=bucket)
+        tag_set = tags['TagSet']
+        
+        is_our_bucket = False
+        for tag in tag_set:
+            if tag['Key'] == 'CreatedBy' and tag['Value'] == 'Hi-platform-cli':
+                is_our_bucket = True
+                break
+        
+        if not is_our_bucket:
+            click.echo(f"Error: Access Denied. Bucket '{bucket}' is not managed by Hi-platform-cli.")
+            sys.exit(1)
+
+        file_name = os.path.basename(file)
+        s3.upload_file(file, bucket, file_name)
+        click.echo("Success! File uploaded.")
+
+    except ClientError as e:
+        click.echo(f"Error: Unable to verify bucket tags. {e}")
+        sys.exit(1)
+    
+    click.echo("Uploading Files...")
+
+
+@s3.command()
 def list():
-    click.echo("Listing Buckets...")
+    click.echo("Listing Buckets based on Tags...")
+    
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+    
+    count = 0
+    if 'Buckets' in response:
+        for bucket in response['Buckets']:
+            bucket_name = bucket['Name']
+            
+            try:
+                tags = s3.get_bucket_tagging(Bucket=bucket_name)
+                tag_set = tags['TagSet']
+                
+                is_our_bucket = False
+                for tag in tag_set:
+                    if tag['Key'] == 'CreatedBy' and tag['Value'] == 'Hi-platform-cli':
+                        is_our_bucket = True
+                        break
+                
+                if is_our_bucket:
+                    click.echo(f"Name: {bucket_name} | Created: {bucket['CreationDate']}")
+                    count += 1
+
+            except ClientError:
+                continue
+
+    click.echo(f"Found {count} Buckets.")
 
 @cli.group()
 def route53():

5ced3f367a33056914b968a0fb8e126c4cbd693d
Hi9841 <maor.azarov@gmail.com>
2026-02-04 12:12:38 +0300

    r53 set

--- main.py
+++ main.py
@@ -8,24 +8,68 @@ from botocore.exceptions import NoCredentialsError, ClientError
 MAX_RESOURCES = 2
 
 def get_global_count():
-    ec2 = boto3.client('ec2')
-    s3 = boto3.client('s3')
+    ec2 = boto3.client('ec2') # declare ec2 client
+    s3 = boto3.client('s3') # declare s3 client
+    r53 = boto3.client('route53') # declare r53 client
 
     filters = [
         {'Name': 'tag:CreatedBy', 'Values': ['Hi-platform-cli']},
         {'Name': 'instance-state-name', 'Values': ['running']}
     ]
     ec2_response = ec2.describe_instances(Filters=filters)
-    ec2_count = sum(len(r['Instances']) for r in ec2_response['Reservations'])
+    # Sum instances from all 'Reservations' == (group of instances)
+    ec2_count = sum(len(r['Instances']) for r in ec2_response.get('Reservations', []))
 
     s3_response = s3.list_buckets()
     s3_count = 0
+    
     if 'Buckets' in s3_response:
         for bucket in s3_response['Buckets']:
-            if bucket['Name'].startswith("hi-platform-cli"):
-                s3_count += 1
+            bucket_name = bucket['Name']
+            try:
+                # asking for tags for each bucket
+                tag_response = s3.get_bucket_tagging(Bucket=bucket_name)
+                tags = tag_response.get('TagSet', [])
+                
+                # if tags exists
+                for tag in tags:
+                    if tag['Key'] == 'CreatedBy' and tag['Value'] == 'Hi-platform-cli':
+                        s3_count += 1
+                        break # found tag
+                        
+            except ClientError as e:
+                # if bucket has no tags
+                if e.response['Error']['Code'] != 'NoSuchTagSet':
+                    print(f"Error checking tags for bucket {bucket_name}: {e}")
+    r53_count = 0
     
-    return ec2_count + s3_count
+    # pagination for multiple zones
+    paginator = r53.get_paginator('list_hosted_zones')
+    
+    for page in paginator.paginate():
+        for zone in page['HostedZones']:
+            zone_id = zone['Id'].split('/')[-1] # id
+            
+            try:
+                # list tags with id
+                tags_response = r53.list_tags_for_resource(
+                    ResourceType='hostedzone', 
+                    ResourceId=zone_id
+                )
+                
+                tags = tags_response['ResourceTagSet']['Tags']
+                
+                # if tags exists
+                for tag in tags:
+                    if tag['Key'] == 'CreatedBy' and tag['Value'] == 'Hi-platform-cli':
+                        r53_count += 1
+                        break # tag found
+                        
+            except Exception as e:
+                print(f"Error checking tags for zone {zone_id}: {e}")
+
+    print(f"Global Count -> EC2: {ec2_count} | S3: {s3_count} | R53: {r53_count}")
+    return ec2_count + s3_count + r53_count
 
 
 @click.group()
@@ -322,8 +366,152 @@ def route53():
     pass
 
 @route53.command()
+@click.option('--domain', required=True, help='The domain name to create (e.g., example.com)')
+def create(domain):
+    click.echo("Creating dns zones...")
+    r53 = boto3.client('route53')
+    ref = str(uuid.uuid4())
+    response = r53.create_hosted_zone(
+        Name=domain,
+        CallerReference=ref,
+        HostedZoneConfig={
+            'Comment': 'string',
+            'PrivateZone': False
+        }
+    )
+    full_id = response['HostedZone']['Id']
+    zone_id = full_id.split('/')[-1]
+    click.echo(f"Create Zone ID: {zone_id}")
+
+    r53.change_tags_for_resource(
+        ResourceType='hostedzone',
+        ResourceId=zone_id,
+        AddTags=[
+            {'Key': 'CreatedBy','Value': 'Hi-platform-cli'}
+        ]
+    )
+
+
+
+@route53.command()
+@click.option('--create', is_flag=True, help="Create a new record")
+@click.option('--update', is_flag=True, help="Update an existing record")
+@click.option('--delete', is_flag=True, help="Delete a record")
+@click.option('--zone', required=True, help="The Domain Zone (e.g. example.com)")
+@click.option('--name', required=True, help="The Record Name (e.g. api.example.com)")
+@click.option('--value', required=True, help="The Value (e.g. 192.168.1.1)")
+@click.option('--type', default='A', help="Record Type (A, CNAME, TXT, etc.)")
+@click.option('--ttl', default=300, help="Time To Live (default: 300)")
+def manage(create, update, delete, zone, name, value, type, ttl):
+    # check if more than 1 flag used
+    if sum([create, update, delete]) != 1:
+        click.echo("Error: You must specify exactly one action: --create, --update, or --delete")
+        return
+
+    r53 = boto3.client('route53')
+    
+    #  zone name with dot format aws adds a . at the end so we add that here as well to match
+    target_zone_name = zone if zone.endswith('.') else f"{zone}."
+    found_zone_id = None
+    
+    # find zone id with name
+    paginator = r53.get_paginator('list_hosted_zones')
+    for page in paginator.paginate():
+        for zone in page['HostedZones']:
+            if zone['Name'] == target_zone_name:
+                found_zone_id = zone['Id'].split('/')[-1]
+                break
+        if found_zone_id:
+            break
+            
+    if not found_zone_id:
+        click.echo(f"Error: Hosted Zone '{zone}' not found.")
+        return
+
+    # check if created by our cli using our tag 
+    is_our_record = False
+    try:
+        tags_response = r53.list_tags_for_resource(
+            ResourceType='hostedzone',
+            ResourceId=found_zone_id
+        )
+        for tag in tags_response['ResourceTagSet']['Tags']: 
+            if tag['Key'] == 'CreatedBy' and tag['Value'] == 'Hi-platform-cli':
+                is_our_record = True
+                break
+    except Exception as e:
+        click.echo(f"Error reading tags: {e}")
+        return
+
+    if not is_our_record:
+        click.echo(f"Permission Denied: Zone '{zone}' is not managed by Hi-platform-cli.")
+        return
+
+    # action based on our flags --delete --update --create
+    action = 'DELETE' if delete else 'UPSERT' # UPSERT = UPDATE + INSERT used to not have to deal with record already exists error 
+    # if the record is missing(doesnt exist) UPSERT will act as create and create it 
+    # if the record exists UPSERT will act as update and update it 
+    # dont want to deal with errors one command for 2 flags
+    
+    change_batch = {
+        'Changes': [
+            {
+                'Action': action,
+                'ResourceRecordSet': {
+                    'Name': name,
+                    'Type': type,
+                    'TTL': ttl,
+                    'ResourceRecords': [{'Value': value}]
+                }
+            }
+        ]
+    }
+
+    # changing
+    try:
+        response = r53.change_resource_record_sets(
+            HostedZoneId=found_zone_id,
+            ChangeBatch=change_batch
+        )
+        status = response['ChangeInfo']['Status']
+        click.echo(f"Success! Status: {status}")
+        
+    except Exception as e:
+        click.echo(f"AWS Error: {e}")
+
+
+
+
+@route53.command()
 def list():
     click.echo("Listing Route53...")
+    r53 = boto3.client('route53')
+    zone_paginator = r53.get_paginator('list_hosted_zones')
+
+    for zone_page in zone_paginator.paginate():
+        for zone in zone_page["HostedZones"]:
+            zone_name = zone['Name']
+            zone_id = zone['Id'].split('/')[-1]
+            record_count = zone['ResourceRecordSetCount']
+            is_our_zone = False
+            try:
+                tags_response = r53.list_tags_for_resource(
+                    ResourceType='hostedzone',
+                    ResourceId=zone_id
+                )
+                for tag in tags_response['ResourceTagSet']['Tags']:
+                    if tag['Key'] == 'CreatedBy' and tag['Value'] == 'Hi-platform-cli':
+                        is_our_zone = True
+                        break
+            except Exception:
+                pass # error
+
+            if not is_our_zone:
+                continue
+
+            click.echo(f"zone {zone_name} ")
+            click.echo(f"record {record_count}")
+
 
 if __name__ == '__main__':
     cli()
